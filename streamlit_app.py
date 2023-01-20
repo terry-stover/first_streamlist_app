253c64bbe53fef2a83e58c4dc27fe9adfbae9e7a
terry-stover <103533800+terry-stover@users.noreply.github.com>
2023-01-19 21:03:03 -0800

    Update streamlit_app.py
    full breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit
 
 streamlit.title('My parents new health diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Bacon and Eggs')
-streamlit.text('oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
