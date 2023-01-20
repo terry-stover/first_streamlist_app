64de487a312ae7461055dd8a778219c7ca43a303
terry-stover <103533800+terry-stover@users.noreply.github.com>
2023-01-19 21:06:13 -0800

    Update streamlit_app.py
    add emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@
 import streamlit
 
 streamlit.title('My parents new health diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🫕 3 & Blueberry Oatmeal')
+streamlit.text('🥗, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
+🍜
